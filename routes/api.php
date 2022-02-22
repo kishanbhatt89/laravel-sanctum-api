9f5f7541ef96ac3c8984ba20056e24067f0c55ae
Kishan Bhatt <iamkishanbhatt@gmail.com>
2022-02-22 12:43:21 +0530

    Sanctum added, user and product crud added

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::resource('products',ProductController::class);
+
+// Public Routes
+
+
+Route::post('/register', [AuthController::class, 'register']);    
+Route::post('/login', [AuthController::class, 'login']);    
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+// Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']);    
+    Route::put('/products/{id}', [ProductController::class, 'update']);    
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
